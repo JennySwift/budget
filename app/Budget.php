cdb857119f213a4ab24795dab5d8a699df67b316
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-01 09:26:59 +0000

    Add budget model

---
+++ app/Budget.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Budget extends Model {
+
+	//
+	protected $fillable = ['type'];
+
+	public function tags () {
+		return $this->hasMany('App\Tag');
+	}
+}
