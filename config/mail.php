290ba5970e3b9eac6d6ed69499a37f4b3eb39666
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-06 03:33:47 +0000

    Add email sender address

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'cheezyspaghetti@gmail.com', 'name' => 'Jenny Swift'],
 
 	/*
 	|--------------------------------------------------------------------------
