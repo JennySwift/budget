2f30ebb176f52d5e882590d649f4615647558429
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-16 03:21:50 +0000

    give tags with fixed budget and tags with flex budget different colors

--- app/inc/transactions.php
+++ app/inc/transactions.php
@@ -49,7 +49,7 @@
     
             <tr ng-show="show.tags" class="results-tag-location-container tag-location-container">
                 <td colspan="8">
-                    <li ng-click="removeResultTag(this);" ng-repeat="tag in transaction.tags" ng-class="{'tag-with-budget': tag.fixed_budget !== null || tag.flex_budget !== null, 'tag-without-budget': tag.fixed_budget === null || tag.flex_budget === null}" class="label label-default" data-id="{{tag.id}}" data-allocated-percent="{{tag.allocated_percent}}" data-allocated-fixed="{{tag.allocated_fixed}}" data-allocated_fixed="{{tag.allocated_fixed}}">{{tag.name}}</li>
+                    <li ng-click="removeResultTag(this);" ng-repeat="tag in transaction.tags" ng-class="{'tag-with-fixed-budget': tag.fixed_budget !== null, 'tag-with-flex-budget': tag.flex_budget !== null, 'tag-without-budget': tag.fixed_budget === null || tag.flex_budget === null}" class="label label-default" data-id="{{tag.id}}" data-allocated-percent="{{tag.allocated_percent}}" data-allocated-fixed="{{tag.allocated_fixed}}" data-allocated_fixed="{{tag.allocated_fixed}}">{{tag.name}}</li>
                 </td>
                 <td colspan="1" class="budget-tag-info">{{transaction.allocate}}</td>
             </tr>
