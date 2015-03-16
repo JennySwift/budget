2cc175ffac97a633099532341a22e72bf01103aa
Jenny Swift <cheezyspaghetti@gmail.com>
2015-03-16 10:22:16 +0000

    turn debugbar off again

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -34,7 +34,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Be right back.</div>
+				<div class="title">Site under construction. Please check back soon.</div>
 			</div>
 		</div>
 	</body>
