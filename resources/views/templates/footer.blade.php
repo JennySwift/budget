0aefa032c7c66fac07afea1fea3d94af39edb744
Jenny Swift <cheezyspaghetti@gmail.com>
2015-07-28 09:21:16 +1000

    Delete bootstrap tour code

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -9,18 +9,7 @@
 </footer>
 
 <!-- plugins -->
-<script type="text/javascript" src="tools/angular.min.js"></script>
-<script type="text/javascript" src="tools/jquery.js"></script>
-<script type="text/javascript" src="tools/underscore-min.js"></script>
-<script type="text/javascript" src="tools/tooltipster.min.js"></script>
-<script type="text/javascript" src="tools/bootstrap-tour.min.js"></script>
-<script type="text/javascript" src="tools/bootstrap.min.js"></script>
-<script type="text/javascript" src="tools/date-en-AU.js"></script>
-<script type="text/javascript" src="tools/time.js"></script>
-<script type="text/javascript" src="tools/moment.js"></script>
-<script type="text/javascript" src="tools/mousetrap.min.js"></script>
-<script type="text/javascript" src="tools/checklist-model.js"></script>
-<script type="text/javascript" src="tools/angular-animate.min.js"></script>
+@include('templates/plugins')
 
 
 <!-- my scripts -->
