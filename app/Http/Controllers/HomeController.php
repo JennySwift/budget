7db9f4af68149d89ebf302433d6d40eb1bf54f81
Valentin Prugnaud <valentin@whatdafox.com>
2015-08-25 22:54:51 -0700

    Docblocks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,9 @@ class HomeController extends Controller
     /**
      * Show the application dashboard to the user.
      * GET /
+     * @param FilterRepository $filterRepository
+     * @param TagsRepository $tagsRepository
+     * @param TotalsService $totalsService
      * @return Response
      */
     public function index(FilterRepository $filterRepository, TagsRepository $tagsRepository, TotalsService $totalsService)
