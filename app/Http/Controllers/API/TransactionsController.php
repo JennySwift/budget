f4afbef0b93ac39f49657dc0706a6b6bceb9bdfe
Jenny Swift <cheezyspaghetti@gmail.com>
2015-08-31 12:10:08 +1000

    Get rid of unneeded TransactionsController.php

--- app/Http/Controllers/API/TransactionsController.php
+++ app/Http/Controllers/API/TransactionsController.php
@@ -338,4 +338,19 @@ class TransactionsController extends Controller
             "allocation_totals" => $transaction->getAllocationTotals()
         ];
     }
+
+    /**
+     * Get allocation totals
+     * POST /select/allocationTotals
+     * @param Request $request
+     * @return array
+     */
+    public function getAllocationTotals(Request $request)
+    {
+        $transaction = Transaction::find($request->get('transaction_id'));
+
+        // It is good, but not ideal. Returning an AllocationTotal object that you could eventually use
+        // with a transformer would be a good idea. But it is fine to keep like this if you want :)
+        return $transaction->getAllocationTotals();
+    }
 }
