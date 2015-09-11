e4526ebda7d0257a2967984b979ea321e50fc422
Valentin Prugnaud <valentin@whatdafox.com>
2015-09-11 00:27:33 -0700

    Create first tests, yay

--- app/Http/Routes/totals.php
+++ app/Http/Routes/totals.php
@@ -7,7 +7,7 @@
 Route::post('select/allocationTotals', 'TransactionsController@getAllocationTotals');
 
 // Resources
-Route::resource('totals', 'TotalsController', ['only' => ['index']]);
+//Route::resource('totals', 'TotalsController', ['only' => ['index']]);
 
 // ?
 // Route::post('totals/basicAndBudget', 'TotalsController@index');
---
+++ tests/PagesTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\User;
+
+class PagesTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 * @test
+	 * @return void
+	 */
+	public function it_redirects_the_user_if_not_authenticated()
+	{
+		$response = $this->call('GET', '/');
+
+		$this->assertEquals(302, $response->getStatusCode());
+		$this->assertTrue($response->isRedirection());
+		$this->assertEquals(
+			$this->baseUrl.'/auth/login',
+			$response->getTargetUrl()
+		);
+	}
+
+	/**
+	 * A basic functional test example.
+	 * @test
+	 * @return void
+	 */
+	public function it_can_display_the_homepage()
+	{
+		$user = User::first();
+		$this->be($user);
+
+		// This can be done by $this->visit('/') with Laravel 5.1
+		$response = $this->call('GET', '/');
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assert
+	}
+
+}
