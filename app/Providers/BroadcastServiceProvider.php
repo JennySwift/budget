81a9da94f59e44c1bd0792a4929628fd2fbd33b2
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-12 04:51:17 +1000

    Fix login page load error after upgrade by adding BroadcastServiceProvider.php and running composer update

---
+++ app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class BroadcastServiceProvider
+ * @package App\Providers
+ */
+class BroadcastServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+}
