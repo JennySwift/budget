ed06675a912d221b2467d89da29ee8608d05cdcc
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-19 19:48:08 +1000

    Fix test error

--- tests/API/AutocompleteTest.php
+++ tests/API/AutocompleteTest.php
@@ -4,32 +4,19 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 /**
  * Class AutocompleteTest
+ * todo
  */
 class AutocompleteTest extends TestCase {
 
     use DatabaseTransactions;
 
-    //todo
+    /**
+     * @test
+     * @return void
+     */
+    public function does_something()
+    {
+        
+    }
 
-	/**
-	 * A basic functional test example.
-	 * @test
-	 * @return void
-	 */
-//	public function it_lists_all_the_accounts()
-//	{
-//        $this->logInUser();
-//
-//        $accounts = Account::all();
-//
-//		$response = $this->call('GET', '/api/accounts');
-//        $content = json_decode($response->getContent(), true);
-//
-//		$this->assertEquals(200, $response->getStatusCode());
-//        $this->assertArrayHasKey('name', $content[0]);
-//        $this->assertArrayHasKey('user_id', $content[0]);
-//        $this->assertArrayHasKey('path', $content[0]);
-//        $this->assertContains($accounts->first()->name, $content[0]);
-//        $this->assertContains($accounts->get(1)->name, $content[1]);
-//	}
 }
