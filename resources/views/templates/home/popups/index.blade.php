49703ea3d417bc422f14b345a7c428ba82b4b85a
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-20 09:49:38 +1000

    Fix page load error

--- resources/views/templates/home/popups/index.blade.php
+++ resources/views/templates/home/popups/index.blade.php
@@ -1,5 +1,4 @@
 
 @include('templates.home.popups.allocation-popup')
-@include('templates.home.popups.credits')
 @include('templates.home.popups.edit-transaction')
 @include('templates.home.popups.mass-edit')
