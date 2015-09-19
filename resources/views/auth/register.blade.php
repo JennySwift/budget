07fa81b89c7d6cca829cabd1e027bc877834b20a
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-19 21:41:12 +1000

    Add requirements to registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div id="register">
-        <h1>Register</h1>
 
         <div id="trial-message">
             <p>Want to try it out before registering? You can do so by going <a
@@ -11,6 +10,13 @@
                 <li>Email: jennyswiftcreations@gmail.com</li>
                 <li>Password: abcdefg</li>
             </ul>
+        </div>
+
+        <h1>Register</h1>
+
+        <div id="requirements">
+            <p>I suggest you use this app with either Mozilla Firefox or Google Chrome. You can try it in other browsers but it may not work as well. I don't know if it works at all with Internet Explorer because I am on a Mac.</p>
+            <p>This app may require patience to use. It is still being made.</p>
         </div>
 
         <div class="flex">
