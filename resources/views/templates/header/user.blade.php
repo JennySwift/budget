63f37ea7e07cf97dec3e17ab160c267b438b6d19
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-19 21:07:27 +1000

    Adjust header for playground user

--- resources/views/templates/header/user.blade.php
+++ resources/views/templates/header/user.blade.php
@@ -9,6 +9,6 @@
     <ul class="dropdown-menu" role="menu">
         <!-- if they are a guest they shouldn't see this page anyway, but so that my code will work... -->
         <li><a href="/auth/logout">Logout</a></li>
-        <li><a ng-click="deleteUser()" href="#">Delete account</a></li>
+        {{--<li><a ng-click="deleteUser()" href="#">Delete account</a></li>--}}
     </ul>
 </li>
