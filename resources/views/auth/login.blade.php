c729c7a304f942ceca52cc8090266303fd4aaa01
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-22 09:22:20 +1000

    Work on login and registration messages for playground version

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,22 @@
 @section('content')
     <div id="login">
         <h1>Login</h1>
+
+        <div id="requirements">
+            <p>You can log in with the following email and password. This will give you access to an existing account with pre-populated data. If you want to instead try it out without the pre-populated data, you can register for the demo version
+                <a href="auth/register">here.</a></p>
+            <ul>
+                <li>Email: jennyswiftcreations@gmail.com</li>
+                <li>Password: abcdefg</li>
+            </ul>
+        </div>
+
+        <div id="requirements">
+            <p>I suggest you use this app with either Mozilla Firefox or Google Chrome. You can try it in other browsers but it may not work as well. I don't know if it works at all with Internet Explorer because I am on a Mac.</p>
+            <p>This app may require patience to use. It is still being made.</p>
+        </div>
+
+
         <div class="flex">
             @include('templates.auth.errors')
             @include('templates.auth.login-form')
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,18 +3,11 @@
 @section('content')
     <div id="register">
 
-        <div id="trial-message">
-            <p>Want to try it out before registering? You can do so by going <a
-                        href="http://budget_playground.jennyswiftcreations.com/auth/login">here</a> and logging in with these details:</p>
-            <ul>
-                <li>Email: jennyswiftcreations@gmail.com</li>
-                <li>Password: abcdefg</li>
-            </ul>
-        </div>
-
         <h1>Register</h1>
 
         <div id="requirements">
+            <p>If you want to try out the app with pre-populated data, click <a
+                        href="auth/login">here.</a></p>
             <p>I suggest you use this app with either Mozilla Firefox or Google Chrome. You can try it in other browsers but it may not work as well. I don't know if it works at all with Internet Explorer because I am on a Mac.</p>
             <p>This app may require patience to use. It is still being made.</p>
         </div>
