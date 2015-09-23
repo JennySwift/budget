e5642b763dc85bbdabc909c6ab3ec4903c35beb5
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-24 09:02:40 +1000

    Add docblocks

--- app/Repositories/Filters/GraphsRepository.php
+++ app/Repositories/Filters/GraphsRepository.php
@@ -75,6 +75,12 @@ class GraphsRepository {
         return max($maxIncome, $maxExpenses);
     }
 
+    /**
+     * 
+     * @param $query
+     * @param $date
+     * @return \App\Models\Totals\FilterTotals
+     */
     private function monthTotals($query, $date)
     {
         $queryClone = clone $query;
