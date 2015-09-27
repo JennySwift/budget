94e93ca3ebf466e6a1f3f8fb146fab48a30fef17
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-28 04:30:57 +1000

    Give seeder two more budgets to test speed

--- config/budgets.php
+++ config/budgets.php
@@ -1,5 +1,203 @@
 <?php
 
+$startingDate = '2015-01-01';
+
 return [
-    'types' => ['fixed', 'flex', 'unassigned']
+    'types' => ['fixed', 'flex', 'unassigned'],
+    'seeder1' => [
+        [
+            'type' => 'unassigned',
+            'name' => 'bank fees'
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'business',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'groceries',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'busking',
+            'amount' => 10.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'eating out',
+            'amount' => 10.00,
+            'starting_date' => $startingDate
+        ],
+    ],
+    'seeder2' => [
+        [
+            'type' => 'unassigned',
+            'name' => 'bank fees'
+        ],
+        [
+            'type' => 'unassigned',
+            'name' => 'something'
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'business',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'groceries',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'rent',
+            'amount' => 50,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'licenses',
+            'amount' => 20,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'insurance',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'conferences',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'car',
+            'amount' => 100,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'mobile phone',
+            'amount' => 20,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'petrol',
+            'amount' => 50,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'fixed',
+            'name' => 'sport',
+            'amount' => 20,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'busking',
+            'amount' => 10.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'eating out',
+            'amount' => 10.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'entertainment',
+            'amount' => 5.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'recreation',
+            'amount' => 20.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'holidays',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'gifts',
+            'amount' => 10.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'books',
+            'amount' => 40.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'clothes',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'church',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'equipment',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'guitar',
+            'amount' => 100.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'health',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'miscellaneous',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'music',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'superannuation',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ],
+        [
+            'type' => 'flex',
+            'name' => 'tax',
+            'amount' => 50.00,
+            'starting_date' => $startingDate
+        ]
+    ]
 ];
--- database/seeds/BudgetSeeder.php
+++ database/seeds/BudgetSeeder.php
@@ -9,177 +9,10 @@ use App\Models\Budget;
 
 class BudgetSeeder extends Seeder {
 
-	protected $budgets;
-    protected $startingDate = '2015-01-01';
+//	protected $budgets;
+//    protected $startingDate = '2015-01-01';
 
 	public function __construct() {
-		$this->budgets = [
-            [
-                'type' => 'unassigned',
-                'name' => 'bank fees'
-            ],
-//            [
-//                'type' => 'unassigned',
-//                'name' => 'something'
-//            ],
-            [
-                'type' => 'fixed',
-                'name' => 'business',
-                'amount' => 100,
-                'starting_date' => $this->startingDate
-            ],
-			[
-				'type' => 'fixed',
-				'name' => 'groceries',
-				'amount' => 100,
-				'starting_date' => $this->startingDate
-			],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'rent',
-//                'amount' => 50,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'licenses',
-//                'amount' => 20,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'insurance',
-//                'amount' => 100,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'conferences',
-//                'amount' => 100,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'car',
-//                'amount' => 100,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'mobile phone',
-//                'amount' => 20,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'petrol',
-//                'amount' => 50,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'fixed',
-//                'name' => 'sport',
-//                'amount' => 20,
-//                'starting_date' => $this->startingDate
-//            ],
-            [
-                'type' => 'flex',
-                'name' => 'busking',
-                'amount' => 10.00,
-                'starting_date' => $this->startingDate
-            ],
-			[
-				'type' => 'flex',
-				'name' => 'eating out',
-				'amount' => 10.00,
-				'starting_date' => $this->startingDate
-			],
-//            [
-//                'type' => 'flex',
-//                'name' => 'entertainment',
-//                'amount' => 5.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'recreation',
-//                'amount' => 20.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'holidays',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'gifts',
-//                'amount' => 10.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'books',
-//                'amount' => 40.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'clothes',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'church',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'equipment',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'guitar',
-//                'amount' => 100.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'health',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'miscellaneous',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'music',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'superannuation',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ],
-//            [
-//                'type' => 'flex',
-//                'name' => 'tax',
-//                'amount' => 50.00,
-//                'starting_date' => $this->startingDate
-//            ]
-
-		];
 
     }
 
@@ -188,7 +21,15 @@ class BudgetSeeder extends Seeder {
 		$users = User::all();
 
 		foreach($users as $user) {
-			foreach($this->budgets as $budget) {
+            /**
+             * @VP:
+             * Why didn't this syntax work?
+             * $budgets = Config::get('budgets.seeder{$user->id}');
+             */
+            
+            $budgets = Config::get('budgets.seeder' . $user->id);
+
+			foreach($budgets as $budget) {
 				$tmp = new Budget($budget);
 				$tmp->user()->associate($user);
 				$tmp->save();
