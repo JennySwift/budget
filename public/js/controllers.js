083fe6134d5b87ad6f3a19d34c80bbc3eecbb821
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-28 14:38:05 +1000

    Cleanup

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -482,22 +482,6 @@ var app = angular.module('budgetApp');
             };
         }
 
-        else if (page === 'unassignedBudgets') {
-            //$scope.unassignedBudgetTotals = unassignedBudgetTotals;
-
-            //$scope.getUnassignedBudgetTotals = function () {
-            //    $scope.showLoading();
-            //    TotalsFactory.getUnassignedBudgetTotals()
-            //        .then(function (response) {
-            //            $scope.unassignedBudgetTotals = response.data;
-            //            $scope.hideLoading();
-            //        })
-            //        .catch(function (response) {
-            //            $scope.responseError(response);
-            //        });
-            //};
-        }
-
         $scope.show.basic_totals = true;
         $scope.show.budget_totals = true;
         $scope.newBudget = {
@@ -516,7 +500,7 @@ var app = angular.module('budgetApp');
             $budget.sql_starting_date = $scope.formatDate($budget.starting_date);
             BudgetsFactory.insert($budget)
                 .then(function (response) {
-                    $scope.jsInsertBudget(response);
+                    jsInsertBudget(response);
                     $scope.getSideBarTotals();
                     $scope.provideFeedback('Budget created');
 
@@ -526,9 +510,6 @@ var app = angular.module('budgetApp');
                     else if ($budget.type === 'flex' && page === 'flexBudgets') {
                         $scope.getFlexBudgetTotals();
                     }
-                    //else if ($budget.type === 'unassigned' && page === 'unassignedBudgets') {
-                    //    $scope.getUnassignedBudgetTotals();
-                    //}
 
                     $scope.hideLoading();
                 })
@@ -540,7 +521,7 @@ var app = angular.module('budgetApp');
         /**
         * Add the budget to the JS array
         */
-        $scope.jsInsertBudget = function (response) {
+        function jsInsertBudget (response) {
             var $budget = response.data.data;
             if ($budget.type === 'fixed' && page === 'fixedBudgets') {
                 $scope.fixedBudgets.push($budget);
@@ -551,7 +532,7 @@ var app = angular.module('budgetApp');
             else if ($budget.type === 'unassigned' && page === 'unassignedBudgets') {
                 $scope.unassignedBudgets.push($budget);
             }
-        };
+        }
 
         /**
         * For updating budget (name, type, amount, starting date) for an existing budget
@@ -562,7 +543,7 @@ var app = angular.module('budgetApp');
             $scope.budget_popup.sqlStartingDate = $scope.formatDate($scope.budget_popup.formattedStartingDate);
             BudgetsFactory.update($scope.budget_popup)
                 .then(function (response) {
-                    $scope.jsUpdateBudget(response);
+                    jsUpdateBudget(response);
                     $scope.getSideBarTotals();
                     $scope.show.popups.budget = false;
                 })
@@ -571,7 +552,7 @@ var app = angular.module('budgetApp');
                 });
         };
 
-        $scope.jsUpdateBudget = function (response) {
+        function jsUpdateBudget (response) {
             //todo: allow for if budget type is changed. I will have to remove the budget from the table it was in
             var $budget = response.data;
             if ($budget.type === 'flex') {
@@ -582,8 +563,7 @@ var app = angular.module('budgetApp');
                 var $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: response.data.id}));
                 $scope.fixedBudgets[$index] = response.data;
             }
-
-        };
+        }
 
         $scope.deleteBudget = function ($budget) {
             $scope.showLoading();
@@ -592,7 +572,7 @@ var app = angular.module('budgetApp');
                 BudgetsFactory.destroy($budget)
                     .then(function (response) {
                         $scope.getSideBarTotals();
-                        $scope.jsDeleteBudget($budget);
+                        jsDeleteBudget($budget);
                         $scope.hideLoading();
                         $scope.provideFeedback('Budget deleted');
                     })
@@ -605,71 +585,28 @@ var app = angular.module('budgetApp');
             }
         };
 
-        $scope.jsDeleteBudget = function ($budget) {
+        function jsDeleteBudget ($budget) {
+            var $index;
+
             if ($budget.type === 'fixed') {
-                var $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: $budget.id}));
                 $scope.fixedBudgets = _.without($scope.fixedBudgets, $budget);
             }
             else if ($budget.type === 'flex') {
-                var $index = _.indexOf($scope.flexBudgets, _.findWhere($scope.flexBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.flexBudgets, _.findWhere($scope.flexBudgets, {id: $budget.id}));
                 $scope.flexBudgets = _.without($scope.flexBudgets, $budget);
             }
             else if ($budget.type === 'unassigned') {
-                var $index = _.indexOf($scope.unassignedBudgets, _.findWhere($scope.unassignedBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.unassignedBudgets, _.findWhere($scope.unassignedBudgets, {id: $budget.id}));
                 $scope.unassignedBudgets = _.without($scope.unassignedBudgets, $budget);
             }
-
-        };
+        }
 
         $scope.showBudgetPopup = function ($tag, $type) {
             $scope.budget_popup = $tag;
             $scope.budget_popup.type = $type;
             $scope.show.popups.budget = true;
         };
-
-        //$scope.updateSavingsTotal = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.updatesavingsTotal()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.show.savings_total.input = false;
-        //            $scope.show.savings_total.edit_btn = true;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
-
-        //$scope.addFixedToSavings = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.addFixedToSavings()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
-
-        //$scope.addPercentageToSavings = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.addPercentageToSavings()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
 
     }
 
--- resources/assets/js/controllers/BudgetsController.js
+++ resources/assets/js/controllers/BudgetsController.js
@@ -59,22 +59,6 @@
             };
         }
 
-        else if (page === 'unassignedBudgets') {
-            //$scope.unassignedBudgetTotals = unassignedBudgetTotals;
-
-            //$scope.getUnassignedBudgetTotals = function () {
-            //    $scope.showLoading();
-            //    TotalsFactory.getUnassignedBudgetTotals()
-            //        .then(function (response) {
-            //            $scope.unassignedBudgetTotals = response.data;
-            //            $scope.hideLoading();
-            //        })
-            //        .catch(function (response) {
-            //            $scope.responseError(response);
-            //        });
-            //};
-        }
-
         $scope.show.basic_totals = true;
         $scope.show.budget_totals = true;
         $scope.newBudget = {
@@ -93,7 +77,7 @@
             $budget.sql_starting_date = $scope.formatDate($budget.starting_date);
             BudgetsFactory.insert($budget)
                 .then(function (response) {
-                    $scope.jsInsertBudget(response);
+                    jsInsertBudget(response);
                     $scope.getSideBarTotals();
                     $scope.provideFeedback('Budget created');
 
@@ -103,9 +87,6 @@
                     else if ($budget.type === 'flex' && page === 'flexBudgets') {
                         $scope.getFlexBudgetTotals();
                     }
-                    //else if ($budget.type === 'unassigned' && page === 'unassignedBudgets') {
-                    //    $scope.getUnassignedBudgetTotals();
-                    //}
 
                     $scope.hideLoading();
                 })
@@ -117,7 +98,7 @@
         /**
         * Add the budget to the JS array
         */
-        $scope.jsInsertBudget = function (response) {
+        function jsInsertBudget (response) {
             var $budget = response.data.data;
             if ($budget.type === 'fixed' && page === 'fixedBudgets') {
                 $scope.fixedBudgets.push($budget);
@@ -128,7 +109,7 @@
             else if ($budget.type === 'unassigned' && page === 'unassignedBudgets') {
                 $scope.unassignedBudgets.push($budget);
             }
-        };
+        }
 
         /**
         * For updating budget (name, type, amount, starting date) for an existing budget
@@ -139,7 +120,7 @@
             $scope.budget_popup.sqlStartingDate = $scope.formatDate($scope.budget_popup.formattedStartingDate);
             BudgetsFactory.update($scope.budget_popup)
                 .then(function (response) {
-                    $scope.jsUpdateBudget(response);
+                    jsUpdateBudget(response);
                     $scope.getSideBarTotals();
                     $scope.show.popups.budget = false;
                 })
@@ -148,7 +129,7 @@
                 });
         };
 
-        $scope.jsUpdateBudget = function (response) {
+        function jsUpdateBudget (response) {
             //todo: allow for if budget type is changed. I will have to remove the budget from the table it was in
             var $budget = response.data;
             if ($budget.type === 'flex') {
@@ -159,8 +140,7 @@
                 var $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: response.data.id}));
                 $scope.fixedBudgets[$index] = response.data;
             }
-
-        };
+        }
 
         $scope.deleteBudget = function ($budget) {
             $scope.showLoading();
@@ -169,7 +149,7 @@
                 BudgetsFactory.destroy($budget)
                     .then(function (response) {
                         $scope.getSideBarTotals();
-                        $scope.jsDeleteBudget($budget);
+                        jsDeleteBudget($budget);
                         $scope.hideLoading();
                         $scope.provideFeedback('Budget deleted');
                     })
@@ -182,71 +162,28 @@
             }
         };
 
-        $scope.jsDeleteBudget = function ($budget) {
+        function jsDeleteBudget ($budget) {
+            var $index;
+
             if ($budget.type === 'fixed') {
-                var $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.fixedBudgets, _.findWhere($scope.fixedBudgets, {id: $budget.id}));
                 $scope.fixedBudgets = _.without($scope.fixedBudgets, $budget);
             }
             else if ($budget.type === 'flex') {
-                var $index = _.indexOf($scope.flexBudgets, _.findWhere($scope.flexBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.flexBudgets, _.findWhere($scope.flexBudgets, {id: $budget.id}));
                 $scope.flexBudgets = _.without($scope.flexBudgets, $budget);
             }
             else if ($budget.type === 'unassigned') {
-                var $index = _.indexOf($scope.unassignedBudgets, _.findWhere($scope.unassignedBudgets, {id: $budget.id}));
+                $index = _.indexOf($scope.unassignedBudgets, _.findWhere($scope.unassignedBudgets, {id: $budget.id}));
                 $scope.unassignedBudgets = _.without($scope.unassignedBudgets, $budget);
             }
-
-        };
+        }
 
         $scope.showBudgetPopup = function ($tag, $type) {
             $scope.budget_popup = $tag;
             $scope.budget_popup.type = $type;
             $scope.show.popups.budget = true;
         };
-
-        //$scope.updateSavingsTotal = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.updatesavingsTotal()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.show.savings_total.input = false;
-        //            $scope.show.savings_total.edit_btn = true;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
-
-        //$scope.addFixedToSavings = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.addFixedToSavings()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
-
-        //$scope.addPercentageToSavings = function ($keycode) {
-        //    if ($keycode !== 13) {
-        //        return;
-        //    }
-        //    savings.addPercentageToSavings()
-        //        .then(function (response) {
-        //            $scope.totals.basic.savings_total = response.data;
-        //            $scope.getTotals();
-        //        })
-        //        .catch(function (response) {
-        //            FeedbackFactory.provideFeedback('There was an error');
-        //        });
-        //};
 
     }
 
