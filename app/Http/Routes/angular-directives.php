92fbe0d3339f35bb01328fb2229857ed0575d52a
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-29 20:57:09 +1000

    Cleanup

--- app/Http/Routes/angular-directives.php
+++ app/Http/Routes/angular-directives.php
@@ -3,11 +3,3 @@
 /**
  * Angular directive templates
  */
-
-Route::get('filter', function () {
-    return view('directives/FilterTemplate');
-});
-
-Route::get('filter-dropdowns', function () {
-    return view('directives/FilterDropdownsTemplate');
-});
