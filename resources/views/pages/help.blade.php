9b38a589fb3b7f9baa6bc174298a927496b556ff
Jenny Swift <cheezyspaghetti@gmail.com>
2015-10-02 15:19:16 +1000

    Get rid of duplicate header on help page

--- resources/views/pages/help.blade.php
+++ resources/views/pages/help.blade.php
@@ -5,8 +5,7 @@ but it's so I have a value for the master template--}}
 @section('controller', 'HelpController')
 
 @section('page-content')
-
-    @include('templates.shared.header')
+    
     @include('templates.help.help-navigation')
 
     <h1>Help</h1>
