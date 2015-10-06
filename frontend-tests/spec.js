55397aeed1582cac3d4fffd822394d2349f29ec3
Jenny Swift <cheezyspaghetti@gmail.com>
2015-10-06 23:16:49 +1100

    More unsuccessful testing attempts

--- frontend-tests/spec.js
+++ frontend-tests/spec.js
@@ -12,14 +12,23 @@ describe('Budget App', function() {
         expect(browser.getTitle()).toEqual('Budget App');
     });
 
-    it('should be able to click the test button', function () {
-        var button = element(by.model('testBtn'));
+    //it('should be able to click the test button', function () {
+        //var button = element(by.model('testBtn'));
         //var button = element(by.id('test-btn'));
-        button.click();
-    });
+        //button.click();
+    //});
     
     it('should be able to fill in an input', function () {
-        var input = element(by.id('test-input'));
+        //Example from docs but 'until' is undefined and 'driver' was undefined
+        //unit I instead used 'browser.driver.'
+        //var input = browser.driver.wait(until.elementLocated(By.id('test-input')), 10000);
+
+        var input = browser.driver.wait(function () {
+            //Apparently this object has no method 'isPresent.'
+            return element(by.id('test-input').isPresent())
+        }, 10000);
+
+        //var input = element(by.id('test-input'));
         input.sendKeys('hi there');
     });
 });
