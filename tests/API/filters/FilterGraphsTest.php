7fdb93499d2a51dc7237f6f594b1e5b13e909ff7
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-13 18:38:02 +1100

    More filter tests

---
+++ tests/API/filters/FilterGraphsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Todo
+ * Class FilterGraphsTest
+ */
+class FilterGraphsTest extends FiltersTest
+{
+    use DatabaseTransactions;
+
+}
