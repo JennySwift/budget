caaf8f8e28860109c66111a24656ee89a9f17b9f
Jenny Swift <cheezyspaghetti@gmail.com>
2016-02-25 15:35:20 +1100

    Start switching to Vue.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,32 +15,24 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('style.scss');
 
-    //mix.scripts([
-    //    'plugins/*.js',
-    //    'app.js',
-    //    'controllers/*.js',
-    //    'filters/*.js',
-    //    'factories/*.js',
-    //    'directives/*.js'
-    //], 'public/js/all.js');
-
     mix.scripts([
         'plugins/*.js',
+        //'config.js',
         'shared/**/*.js',
         'accounts/*.js',
         'favourite-transactions/*.js',
         'budgets/*.js',
         'help/*.js',
         'home/**/*.js',
-        'preferences/*.js'
+        'preferences/*.js',
+        //'repositories/**/*.js',
+        //'components/**/*.js',
+        'app.js',
+
+
     ], 'public/js/all.js');
 
+    mix.version(["css/style.css", "js/all.js"]);
     mix.stylesIn('resources/assets/css', 'public/css/plugins.css');
-
-    //mix.scripts('resources/assets/js/app.js', 'public/js/app.js');
-    //mix.scriptsIn('resources/assets/js/filters', 'public/js/filters.js');
-    //mix.scriptsIn('resources/assets/js/controllers', 'public/js/controllers.js');
-    //mix.scriptsIn('resources/assets/js/factories', 'public/js/factories.js');
-    //mix.scriptsIn('resources/assets/js/directives', 'public/js/directives.js');
-    //mix.scriptsIn('resources/assets/js/plugins', 'public/js/plugins.js');
 });
+
