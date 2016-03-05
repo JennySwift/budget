c5ac7d3568b5801e3bf984fc6b3dc49761c446d0
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-05 13:45:32 +1100

    Refactor html for amount filter

--- resources/views/main/filter/totals-filter-component.blade.php
+++ resources/views/main/filter/totals-filter-component.blade.php
@@ -6,46 +6,67 @@
 
         <div class="content">
 
-            <span>Negative sign required for negative numbers</span>
-
             <div
-                    v-show="filterTab === 'show'"
-                    v-if="filter.total"
-                    class="group">
-                <input
-                        v-model="filter.total.in"
-                        v-on:keyup="filterTotal($event.keyCode)"
-                        type="text"
-                        placeholder="amount">
+                v-show="filterTab === 'show'"
+                v-if="filter.total"
+                class="form-group"
+            >
 
-            <span class="input-group-btn">
-                <button
-                        v-on:click="clearFilterField('total', 'in')"
-                        class="clear-search-button"
-                        type="button">
-                    clear
-                </button>
-            </span>
+                <label for="filter-total-in">Filter in by total (negative sign required for expenses)</label>
+
+                <div
+                    class="input-group"
+                >
+                    <input
+                            v-model="filter.total.in"
+                            v-on:keyup.13="filterTotal()"
+                            type="text"
+                            id="filter-total-in"
+                            name="filter-total-in"
+                            placeholder="total"
+                            class="form-control"
+                    >
+                    <span class="input-group-btn">
+                         <button
+                                 v-on:click="clearFilterField('total', 'in')"
+                                 class="clear-search-button btn btn-default"
+                                 type="button">
+                             clear
+                         </button>
+                    </span>
+                </div>
+
             </div>
 
             <div
-                    v-show="filterTab === 'hide'"
-                    v-if="filter.total"
-                    class="group">
-                <input
-                        v-model="filter.total.out"
-                        v-on:keyup="filterTotal($event.keyCode)"
-                        type="text"
-                        placeholder="amount">
+                v-show="filterTab === 'hide'"
+                v-if="filter.total"
+                class="form-group">
 
-            <span class="input-group-btn">
-                <button
-                        v-on:click="clearFilterField('total', 'out')"
-                        class="clear-search-button"
-                        type="button">
-                    clear
-                </button>
-            </span>
+                <label for="filter-total-out">Filter out by total (negative sign required for expenses)</label>
+
+                <div
+                        class="input-group"
+                >
+                    <input
+                            v-model="filter.total.out"
+                            v-on:keyup.13="filterTotal()"
+                            type="text"
+                            id="filter-total-out"
+                            name="filter-total-out"
+                            placeholder="total"
+                            class="form-control"
+                    >
+                    <span class="input-group-btn">
+                         <button
+                                 v-on:click="clearFilterField('total', 'out')"
+                                 class="clear-search-button btn btn-default"
+                                 type="button">
+                             clear
+                         </button>
+                    </span>
+                </div>
+
             </div>
 
         </div>
