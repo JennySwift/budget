eca4ab04b853b57fd3b572660d395026f5231074
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-05 16:09:48 +1100

    Refactor html for num budgets filter

--- resources/views/main/filter/num-budgets-filter-component.blade.php
+++ resources/views/main/filter/num-budgets-filter-component.blade.php
@@ -6,84 +6,128 @@
 
         <div class="content budget">
 
-            <div v-show="filterTab === 'show'">
-                <input
-                        v-model="filter.numBudgets.in"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsIn"
-                        value="all">
-                <label for="">All</label>
-            </div>
+            {{--Filter in--}}
 
             <div v-show="filterTab === 'show'">
-                <input
-                        v-model="filter.numBudgets.in"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsIn"
-                        value="zero">
-                <label for="">No budgets</label>
+
+                {{--Filter in all--}}
+                <div class="radio inline">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.in"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-in"
+                            value="all"
+                        >
+                        Any number of budgets
+                    </label>
+                </div>
+
+                {{--Filter in transactions with no budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                                v-model="filter.numBudgets.in"
+                                v-on:change="runFilter()"
+                                type="radio"
+                                name="filter-num-budgets-in"
+                                value="zero"
+                        >
+                        No budgets
+                    </label>
+                </div>
+
+                {{--Filter in transactions with single budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.in"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-in"
+                            value="single"
+                        >
+                            One budget
+                    </label>
+                </div>
+
+                {{--Filter in transactions with multiple budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.in"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-in"
+                            value="multiple"
+                        >
+                            Multiple budgets
+                    </label>
+                </div>
+
             </div>
+
+            {{--Filter out--}}
 
             <div v-show="filterTab === 'hide'">
-                <input
-                        v-model="filter.numBudgets.out"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsOut"
-                        value="none">
-                <label for="">None (Do not filter out)</label>
-            </div>
 
-            <div v-show="filterTab === 'hide'">
-                <input
-                        v-model="filter.numBudgets.out"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsOut"
-                        value="zero">
-                <label for="">No budgets</label>
-            </div>
+                {{--Filter out none--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.out"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-out"
+                            value="none"
+                        >
+                            Do not filter out by number of budgets
+                    </label>
+                </div>
 
-            <div v-show="filterTab === 'show'">
-                <input
-                        v-model="filter.numBudgets.in"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsIn"
-                        value="single">
-                <label for="">Single</label>
-            </div>
+                {{--Filter out transactions with no budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.out"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-out"
+                            value="zero"
+                        >
+                            No budgets
+                    </label>
+                </div>
 
-            <div v-show="filterTab === 'hide'">
-                <input
-                        v-model="filter.numBudgets.out"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsOut"
-                        value="single">
-                <label for="">Single</label>
-            </div>
+                {{--Filter out transactions with single budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.out"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-out"
+                            value="single"
+                        >
+                            One budget
+                    </label>
+                </div>
 
-            <div v-show="filterTab === 'show'">
-                <input
-                        v-model="filter.numBudgets.in"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsIn"
-                        value="multiple">
-                <label for="">Multiple</label>
-            </div>
+                {{--Filter out transactions with multiple budgets--}}
+                <div class="radio">
+                    <label>
+                        <input
+                            v-model="filter.numBudgets.out"
+                            v-on:change="runFilter()"
+                            type="radio"
+                            name="filter-num-budgets-out"
+                            value="multiple"
+                        >
+                            Multiple budgets
+                    </label>
+                </div>
 
-            <div v-show="filterTab === 'hide'">
-                <input
-                        v-model="filter.numBudgets.out"
-                        v-on:change="runFilter()"
-                        type="radio"
-                        name="numBudgetsOut"
-                        value="multiple">
-                <label for="">Multiple</label>
             </div>
 
         </div>
