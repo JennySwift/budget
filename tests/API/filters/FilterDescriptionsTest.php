006f9df870980104a33272a79b53cb7b315a70c1
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-09 10:31:14 +1100

    Write test for description filter out

--- tests/API/filters/FilterDescriptionsTest.php
+++ tests/API/filters/FilterDescriptionsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Transaction;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\Response;
 
@@ -66,6 +67,48 @@ class FilterDescriptionsTest extends FiltersTest
         foreach ($this->transactions as $transaction) {
             $this->assertNotContains('e', $transaction['description'], '', true);
         }
+
+        $this->assertEquals(Response::HTTP_OK, $this->response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_does_not_filter_out_more_transactions_than_it_should_when_the_description_filter_out_is_used()
+    {
+        $this->setFilterDefaults();
+        $this->logInUser();
+
+        //Count the number of transactions that do contain 'e' in the description
+        //Todo: perhaps make seeder consistent so it's the same each time
+
+        $count = Transaction::forCurrentUser()->where('description', 'LIKE', '%e%')->count();
+
+        $filter = [
+            'description' => [
+                'in' => '',
+                'out' => 'e'
+            ]
+        ];
+
+        $this->filter = array_merge($this->defaults, $filter);
+
+        $data = [
+            'filter' => $this->filter
+        ];
+        $this->setTransactions($data);
+
+        $this->checkTransactionKeysExist($this->transactions[0]);
+
+        foreach ($this->transactions as $transaction) {
+            if ($transaction['merchant']) {
+                $this->assertNotContains('e', $transaction['description'], '', true);
+            }
+        }
+
+        //There are 16 transactions in total, so there should be 16 - $count in the filtered results
+        $this->assertCount(16 - $count, $this->transactions);
 
         $this->assertEquals(Response::HTTP_OK, $this->response->getStatusCode());
     }
