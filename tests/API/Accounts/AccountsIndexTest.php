85d09f5cd122d4a700427bbfded280d7b8853258
Jenny Swift <cheezyspaghetti@gmail.com>
2016-05-15 19:42:26 +1000

    Refactor account tests

---
+++ tests/API/Accounts/AccountsIndexTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Account;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
+
+class AccountsIndexTest extends TestCase {
+
+    use DatabaseTransactions;
+
+    /**
+     * @todo: check the accounts belong to the user
+     * @test
+     * @return void
+     */
+    public function it_gets_the_accounts()
+    {
+        $this->logInUser();
+        $response = $this->call('GET', '/api/accounts');
+        $content = json_decode($response->getContent(), true);
+//      dd($content);
+
+        $this->checkAccountKeysExist($content[0]);
+
+        $this->assertEquals('bank account', $content[0]['name']);
+        $this->assertEquals('cash', $content[1]['name']);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
