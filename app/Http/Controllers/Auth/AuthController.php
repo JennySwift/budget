3049d07486d7014b41e600945b89c4cb1f8d2c38
Jenny Swift <cheezyspaghetti@gmail.com>
2016-05-15 17:59:35 +1000

    Fix error when new user registers

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
-	protected $redirectTo = '/help';
+	protected $redirectTo = '/';
 
 	/**
 	 * Create a new authentication controller instance.
