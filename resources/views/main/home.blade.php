a78a9dae7012c52dfbad8f6e136aa65e98182a28
Jenny Swift <cheezyspaghetti@gmail.com>
2017-10-26 14:59:33 +1100

    Get hot reloading working

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -28,7 +28,8 @@
 
 {{--Google Analytics--}}
 @include('templates.analytics-tracking')
-<script type="text/javascript" src="/js/app.js"></script>
+{{--<script type="text/javascript" src="/js/app.js"></script>--}}
+<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 
 </body>
 </html>
