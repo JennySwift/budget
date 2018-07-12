4dc99c195b464ac8e9c7c2fdc8bb6a6fd6038a09
Jenny Swift <cheezyspaghetti@gmail.com>
2018-07-12 11:40:20 +1000

    Fix login page error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,9 +55,9 @@
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
+                                {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
+                                    {{--Forgot Your Password?--}}
+                                {{--</a>--}}
                             </div>
                         </div>
                     </form>
